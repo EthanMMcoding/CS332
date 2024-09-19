e40e077f805738a9840f0a324527fe25d95fc3bb
Ethan Martignoni <ethanmartignoni287@gmail.com>
2024-09-19 11:50:46 -0500

    finished Lab04

---
+++ Lab04/Lab04.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFF_SIZE 4096
+
+int main(int argc, char *argv[]){
+  int read_file_descriptor, write_file_descriptor;
+  char buf[BUFF_SIZE];
+  long int n;
+  long int x;
+
+  if (argc != 3){
+		printf("Usage: %s <source_filename> <destination_filename>\n", argv[0]);
+		exit (-1);
+	}
+
+  write_file_descriptor = open(argv[1], O_WRONLY|O_APPEND);
+  read_file_descriptor = open(argv[2], O_RDONLY);
+
+  if (read_file_descriptor == -1 || write_file_descriptor == -1){
+		printf("Error with file open\n");
+		exit (-1);
+	}
+
+  while((n = read(read_file_descriptor, buf, BUFF_SIZE)) > 0){
+    if((x = write(write_file_descriptor, buf, n)) != n){
+      printf("n is %lu, write is %lu\n", n, x);
+      printf("Error writing to output file\n");
+			exit (-1);
+    }
+  }
+  if (n < 0){
+		printf("Error reading input file\n");
+		exit (-1);
+	}
+
+  close(read_file_descriptor);
+	close(write_file_descriptor);
+
+  return 0;
+}
