175ababf02ea4500be7fdfe617a6ffeacdddcf7c
Ethan Martignoni <ethanmartignoni287@gmail.com>
2024-10-04 12:35:21 -0500

    cleaned up include statement in sort.h

---
+++ Homework02/sort.h
@@ -0,0 +1,19 @@
+#ifndef OPTIONS
+
+#include <stdio.h>
+#include "file_struct.h"
+
+void sort(file *file_arr, size_t size){
+  for(int i = 1; i < size; i++){
+    file file_i = *(file_arr + i);
+    long key = file_i.file_stat.st_size;
+    int j = i - 1;
+    while(j >= 0 && (*(file_arr + j)).file_stat.st_size < key){
+      *(file_arr + j + 1) = *(file_arr + j);
+      j--;
+    }
+    *(file_arr + j + 1) = file_i;
+  }
+}
+
+#endif
