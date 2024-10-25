886e5096630c64d2ec41b8e601bdee51280f7dee
Ethan Martignoni <ethanmartignoni287@gmail.com>
2024-10-25 12:50:21 -0500

    lab09

--- lab09/lab09.c
+++ lab09/lab09.c
@@ -6,11 +6,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+pid_t pid;
+
+static void handle_parent_interupts(int signo){
+  
+  switch(signo){
+    case SIGINT:
+      printf("\nParent overriding SIGINT signal %d, waiting for ctrl+\\ \n", signo);
+      break;
+    case SIGTSTP:
+      printf("\nParent overriding SIGTSTP signal %d, waiting for ctrl+\\ \n", signo);
+      break;
+    default:
+      printf("Recieved signal %d\n", signo);
+      break;
+  }
+  fflush(stdout);
+  signal(signo, handle_parent_interupts);
+}
+
 int main(int argc, char **argv) {
-    pid_t pid;
     int status;
 
     if (argc < 2) {
@@ -22,9 +41,15 @@ int main(int argc, char **argv) {
     if (pid == 0) { /* this is child process */
         execvp(argv[1], &argv[1]);
         printf("If you see this statement then execl failed ;-(\n");
-        perror("execvp");
-        exit(-1);
+	      perror("execvp");
+	      exit(-1);
     } else if (pid > 0) { /* this is the parent process */
+        if (signal(SIGINT, handle_parent_interupts) == SIG_ERR){
+          printf("Unable to catch SIGINT\n");
+        }
+        if (signal(SIGTSTP, handle_parent_interupts) == SIG_ERR){
+          printf("Unable to catch SIGTSTP\n");
+        }
         printf("Wait for the child process to terminate\n");
         wait(&status); /* wait for the child process to terminate */
         if (WIFEXITED(status)) { /* child process terminated normally */
